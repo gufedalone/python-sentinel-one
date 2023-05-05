52a9998d1440e7d5619c9533b2b5a35ea6ba5374
vlad <khalvlalove@gmail.com>
2023-05-06 03:02:39 +0400

    clean up

--- controls/dropdown.py
+++ controls/dropdown.py
@@ -1,4 +1,4 @@
-from selene import have
+from selene.support.conditions import have
 from selene.core.entity import Element
 from selene.support.shared import browser
 
@@ -10,6 +10,3 @@ class Dropdown:
 
     def select(self, /, *, option: str):
         browser.all('option').element_by(have.exact_text(option)).click()
-
-    #def autocomplete(self: Element, /, *, option: str):
-    #    self.element('option').set_value(option).press_tab()
